b125fb9187c5e87ba813d02facfbd5eef398b6dc
Olivia Kaufmann <Olivia Kaufmann>
2016-04-24 19:07:52 +0200

    ode first pulley

--- ODE.m
+++ ODE.m
@@ -1,18 +1,38 @@
-function [ grad2 ] = ODE( params ) %params
+% function [ grad ] = ODE( params ) %params
 
-l1 = params.l1; l2 = params.l2; l3 = params.l3; l4 = params.l4; l5 = params.l5;
-ang_base = params.ang_base; F = params.F; lr10 = params.lr10; lr20 = params.lr20;
-E_c = params.E_c; mu_c = params.mu_c; d10 = params.d10; d20 = params.d20;
+% l1 = params.l1; l2 = params.l2; l3 = params.l3; l4 = params.l4; l5 = params.l5;
+% ang_base = params.ang_base; F = params.F; lr10 = params.lr10; lr20 = params.lr20;
+% E_c = params.E_c; mu_c = params.mu_c; d10 = params.d10; d20 = params.d20;
 
 syms lr1 lr2
-% syms F l1 l2 l3 l4 l5 ang_base lr10 lr20 E_c mu_c d10 d20
+syms F l1 l2 l3 l4 l5 ang_base lr10_start lr20 E_c mu_c d10 d20
+syms I r F_c
 syms dlr1 dlr2
+syms ddlr1 ddlr2
 
-% dlr1 = 0; dlr2 = 0;
+alpha = acos(-((lr2-l1-l5)^2-l2^2-(lr1-l3)^2)/(2*l2*(lr1-l3)));
 
-grad = (F*cos(acos(((l5 - l1 + lr2)^2 - (l3 - lr1)^2 + l2^2)/(2*l2*(l5 - l1 + lr2))) - acos(((l3 - lr1)^2 - (l5 - l1 + lr2)^2 + l2^2)/(2*l2*(l3 - lr1))))*sin(acos(((l5 - l1 + lr2)^2 - (l3 - lr1)^2 + l2^2)/(2*l2*(l5 - l1 + lr2))) - ang_base - pi/2)*(((2*dlr2*(l5 - l1 + lr2) + 2*dlr1*(l3 - lr1))/(2*l2*(l5 - l1 + lr2)) - (dlr2*((l5 - l1 + lr2)^2 - (l3 - lr1)^2 + l2^2))/(2*l2*(l5 - l1 + lr2)^2))/(1 - ((l5 - l1 + lr2)^2 - (l3 - lr1)^2 + l2^2)^2/(4*l2^2*(l5 - l1 + lr2)^2))^(1/2) + ((2*dlr2*(l5 - l1 + lr2) + 2*dlr1*(l3 - lr1))/(2*l2*(l3 - lr1)) - (dlr1*((l3 - lr1)^2 - (l5 - l1 + lr2)^2 + l2^2))/(2*l2*(l3 - lr1)^2))/(1 - ((l3 - lr1)^2 - (l5 - l1 + lr2)^2 + l2^2)^2/(4*l2^2*(l3 - lr1)^2))^(1/2)))/sin(acos(((l5 - l1 + lr2)^2 - (l3 - lr1)^2 + l2^2)/(2*l2*(l5 - l1 + lr2))) - acos(((l3 - lr1)^2 - (l5 - l1 + lr2)^2 + l2^2)/(2*l2*(l3 - lr1))))^2 - (F*cos(acos(((l5 - l1 + lr2)^2 - (l3 - lr1)^2 + l2^2)/(2*l2*(l5 - l1 + lr2))) - ang_base - pi/2)*((2*dlr2*(l5 - l1 + lr2) + 2*dlr1*(l3 - lr1))/(2*l2*(l5 - l1 + lr2)) - (dlr2*((l5 - l1 + lr2)^2 - (l3 - lr1)^2 + l2^2))/(2*l2*(l5 - l1 + lr2)^2)))/(sin(acos(((l5 - l1 + lr2)^2 - (l3 - lr1)^2 + l2^2)/(2*l2*(l5 - l1 + lr2))) - acos(((l3 - lr1)^2 - (l5 - l1 + lr2)^2 + l2^2)/(2*l2*(l3 - lr1))))*(1 - ((l5 - l1 + lr2)^2 - (l3 - lr1)^2 + l2^2)^2/(4*l2^2*(l5 - l1 + lr2)^2))^(1/2)) - (E_c*pi*d10^2*dlr1^2*((mu_c*(- lr1*dlr1 + lr10))/lr10 + 1)^2)/(4*lr10) - (E_c*pi*d10^2*dlr1^2*mu_c*((mu_c*(- lr2*dlr1 + lr10))/lr10 + 1)*(- lr1*dlr1 + lr10))/(2*lr10^2);
+% derivative of alpha wrt lr1
+% alphad = gradient(alpha,lr1);
+alphad = ((l2^2 - (l1 + l5 - lr2)^2 + (l3 - lr1)^2)/(2*l2*(l3 - lr1)^2) - (2*l3 - 2*lr1)/(2*l2*(l3 - lr1)))/(1 - (l2^2 - (l1 + l5 - lr2)^2 + (l3 - lr1)^2)^2/(4*l2^2*(l3 - lr1)^2))^(1/2);
 
-grad2 = solve(grad == 0,dlr1);
+% derivative of alpha wrt t
+Der_alpha = alphad * dlr1;
 
-end
+% 2nd derivative of alpha wrt lr1
+% alphadd = gradient(alphad,lr1);
+alphadd = (1/(l2*(l3 - lr1)) + (l2^2 - (l1 + l5 - lr2)^2 + (l3 - lr1)^2)/(l2*(l3 - lr1)^3) - (2*l3 - 2*lr1)/(l2*(l3 - lr1)^2))/(1 - (l2^2 - (l1 + l5 - lr2)^2 + (l3 - lr1)^2)^2/(4*l2^2*(l3 - lr1)^2))^(1/2) + (((l2^2 - (l1 + l5 - lr2)^2 + (l3 - lr1)^2)/(2*l2*(l3 - lr1)^2) - (2*l3 - 2*lr1)/(2*l2*(l3 - lr1)))*((l2^2 - (l1 + l5 - lr2)^2 + (l3 - lr1)^2)^2/(2*l2^2*(l3 - lr1)^3) - ((2*l3 - 2*lr1)*(l2^2 - (l1 + l5 - lr2)^2 + (l3 - lr1)^2))/(2*l2^2*(l3 - lr1)^2)))/(2*(1 - (l2^2 - (l1 + l5 - lr2)^2 + (l3 - lr1)^2)^2/(4*l2^2*(l3 - lr1)^2))^(3/2));
+
+% 2nd derivative of alpha wrt t
+Der2_alpha = alphadd * dlr1^2 + alphad * ddlr1;
+
+lr10 = lr10_start + r * alpha;
+
+eq = I/r * Der2_alpha + F_c + E_c*(lr1-lr10)/lr10*pi/4*(d10*mu_c*(lr1-lr10)/lr10+d10)^2;
+
+simplify(solve(eq,ddlr1))
+
+
+
+% end
 
